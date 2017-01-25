1930db2936d9d923e9fae72cab7986164a2e5404
michaelciletti <michaelciletti@gmail.com>
2017-01-25 10:53:01 -0500

    first commit

---
+++ addition.rb
@@ -0,0 +1,4 @@
+def add(num1, num2)
+2
+
+end
---
+++ test_addition.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "addition.rb"
+
+class TestAddition < Minitest::Test
+
+def test_1_plus_1_returns_2
+	assert_equal(2, add(1,1))
+	
+	
+end
+
+
+
+end
