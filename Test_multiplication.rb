cfae438db156de0551a0627d717866e20ff8a6ee
michaelciletti <michaelciletti@gmail.com>
2017-01-26 10:21:26 -0500

    test neg times float

--- Test_multiplication.rb
+++ Test_multiplication.rb
@@ -23,4 +23,16 @@ class TestMultiplication < Minitest::Test
 		assert_equal(-6, mult(-2,3))
 	end
 
+	def test_mult_neg2_X_neg3_returns_6
+		assert_equal(6, mult(-2,-3))
+	end
+
+	def test_mult_neg10_X3_returns_neg30
+		assert_equal(-30, mult(-10,3,))
+	end
+
+	def test_mult_neg3_X2point2_returns_neg6point6
+		assert_equal(-6.6, mult(-3,2.2))
+	end
+
 end
