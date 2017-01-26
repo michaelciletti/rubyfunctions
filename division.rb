30812765e02e6e23fd2d73b58399f15d3894d665
michaelciletti <michaelciletti@gmail.com>
2017-01-26 14:35:01 -0500

    still trying but no win with divisor of 0

--- division.rb
+++ division.rb
@@ -1,13 +1,22 @@
 def divide(*numbers)
 	numbers.inject do |divedend, number|
-		quo = (divedend /= number).round(2)
-	if number != 0 
-		return quo
-	else return "error"
+	quo = (divedend /= number).round(2)
 		
-		
-	end
+
+		if number.zero?
+  			return 'Cannot divide by 0!'
+		else
+  			quo
+		end
+
+		# if number != 0 
+		# 	return quo
+		# else 
+		# 	return "error"
+		# end
 
 	end
 
 end
+
+puts divide(10,0)
