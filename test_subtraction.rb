1fedc2703ecbbda6a5cc46091cba47f021c64c8d
michaelciletti <michaelciletti@gmail.com>
2017-01-26 08:55:04 -0500

    more tests

--- test_subtraction.rb
+++ test_subtraction.rb
@@ -7,6 +7,18 @@ class TestSubtraction < Minitest::Test
 		assert_equal(9, sub(10,1))
 	end
 
+	def test_sub_10_minus_1_minus_2_returns_7
+		assert_equal(7, sub(10,1,2))
+	end
+
+	def test_sub_0_minus_4_minus_2_returns_neg7
+		assert_equal(7, sub(10,1,2))
+	end
+
+	def test_sub_100_minus_99_minus_100_returns_neg99
+		assert_equal(7, sub(10,1,2))
+	end
+
 	# def test_sub_10_8_minus_1_7_returns_9_1
 	# 	assert_equal(9.1, sub(10.8,1.7))
 	# end
