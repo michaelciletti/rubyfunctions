146dfb093be6baba311dad398e81261025106422
michaelciletti <michaelciletti@gmail.com>
2017-05-21 21:33:09 -0400

    readme

--- multiplication.rb
+++ multiplication.rb
@@ -1,12 +1,13 @@
-# def mult(*numbers)
-# 	numbers.inject do |product, number|
-# 		(product *= number).round(2)
-# 	end
-# end
+def mult(*numbers)
+	numbers.inject do |product, number|
+		(product *= number).round(2)
+	end
+end
 # ^^^^^Works for all lengths of numbers and floats.
 
 
-def mult(num1, num2)
-	results = ""
-	results = num1 * num2
-end
+# def mult(num1, num2)
+# 	results = ""
+# 	results = num1 * num2
+# end
+# ^^^^^only works with 2 whole numbers.
